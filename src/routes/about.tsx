e4687bc9b365956ea78b33892b6ec2b58236396e
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 11:47:22 +0200

    Implemented about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,31 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  return (
+    <div className="h-screen w-full overflow-y-auto">
+      <div className="flex flex-col items-center justify-center mt-[150px] max-w-2xl mx-auto text-center">
+        <p>
+          This page shows the current birch pollen data for the Stockholm
+          region. The data is processed through a Java backend and displayed
+          using React. The app is built by using
+        </p>
+        <br />
+        <a
+          href="https://api.pollenrapporten.se/"
+          target="_blank"
+          className="text-blue-500 underline hover:text-blue-700 whitespace-nowrap"
+        >
+          This external API
+        </a>
+        <p className="mt-6">
+          For any inquiries you can contact me at:{" "}
+          <a href="mailto:nikitamokhonko@gmail.com">nikitamokhonko@gmail.com</a>
+        </p>
+      </div>
+    </div>
+  );
 }
