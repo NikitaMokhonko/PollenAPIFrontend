1126d8866ec4d7a7974c34040912c48ea64e8f57
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 11:53:40 +0200

    Redesigned about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -7,20 +7,19 @@ export const Route = createFileRoute("/about")({
 function RouteComponent() {
   return (
     <div className="h-screen w-full overflow-y-auto">
-      <div className="flex flex-col items-center justify-center mt-[150px] max-w-2xl mx-auto text-center">
+      <div className="flex flex-col items-center justify-center mt-[150px] max-w-lg mx-auto text-center">
         <p>
-          This page shows the current birch pollen data for the Stockholm
-          region. The data is processed through a Java backend and displayed
-          using React. The app is built by using
+          The data is collected from{" "}
+          <a
+            href="https://api.pollenrapporten.se/"
+            target="_blank"
+            className="text-blue-500 underline hover:text-blue-700 whitespace-nowrap"
+          >
+            this external API,
+          </a>{" "}
+          processed by a Java backend and displayed using React.
         </p>
         <br />
-        <a
-          href="https://api.pollenrapporten.se/"
-          target="_blank"
-          className="text-blue-500 underline hover:text-blue-700 whitespace-nowrap"
-        >
-          This external API
-        </a>
         <p className="mt-6">
           For any inquiries you can contact me at:{" "}
           <a href="mailto:nikitamokhonko@gmail.com">nikitamokhonko@gmail.com</a>
