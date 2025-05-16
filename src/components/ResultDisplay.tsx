55a74e94eb1fa840b90ac655fc424acb0d496709
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 23:49:19 +0200

    Added images that correspond to every pollen count

--- src/components/ResultDisplay.tsx
+++ src/components/ResultDisplay.tsx
@@ -14,9 +14,39 @@ export const ResultDisplay: React.FC = () => {
 
   if (pollenCount === null) return <div>Loading...</div>;
 
+  let image = "";
+
+  switch (true) {
+    case pollenCount === 1:
+      image = "public/images/PollenCount1.png";
+      break;
+    case pollenCount === 2:
+      image = "public/images/PollenCount2.png";
+      break;
+    case pollenCount === 3:
+      image = "public/images/PollenCount3.png";
+      break;
+    case pollenCount === 4:
+      image = "public/images/PollenCount4.png";
+      break;
+    case pollenCount === 5:
+      image = "public/images/PollenCount5.png";
+      break;
+    case pollenCount === 6:
+      image = "public/images/PollenCount6.png";
+      break;
+    case pollenCount === 7:
+      image = "public/images/PollenCount7.png";
+      break;
+    default:
+  }
+
   return (
     <div>
-      <h1>{pollenCount}</h1>
+      <img className="border-2 border-black rounded-2xl"
+        src={image}
+        alt={`Pollen count: ${pollenCount}`}
+      />
     </div>
   );
 };
