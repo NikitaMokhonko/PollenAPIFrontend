cb5d5e8b0988dfff84edf9418204c7327aed7d71
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 13:19:58 +0200

    Implemented footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigate } from "@tanstack/react-router";
+
+export const Footer: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>© 2025 Nikita Mokhonko</>
+    )
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,14 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
       <Outlet />
-      <TanStackRouterDevtools />
+      <Footer />
     </>
   ),
 });
