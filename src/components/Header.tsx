71f6554efe2ff9c4f1cabd4c9147b30327e4e8a0
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-16 11:11:00 +0200

    Restyled buttons and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,20 @@ export const Header: React.FC = () => {
   return (
     <nav
       className={`
-      fixed flex justify-between w-full h-[100px] py-2 bg-green-700 border-b-2 border-black
+      fixed flex justify-between w-full h-[100px] py-2 bg-green-700 border-b-2 p-4 border-black
       `}
     >
       <button
-        className="hover:cursor-pointer"
+        className="hover:cursor-pointer text-white hover:border-2 hover:border-white p-5"
+        onClick={() => navigate({ to: "/" })}
+      >
+        Home
+      </button>
+      <h1 className="text-center text-white">
+        Birch Pollen Forecast for Stockholm
+      </h1>
+      <button
+        className="hover:cursor-pointer text-white hover:border-2 hover:border-white p-5"
         onClick={() => navigate({ to: "/about" })}
       >
         About
