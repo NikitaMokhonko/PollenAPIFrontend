5732a61319449a71819b5f51808af4489342799b
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-19 14:29:41 +0200

    Removed unused dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import "../App.css";
-import { fetchPollenCount } from "@/utilities/fetcher";
 import { ResultDisplay } from "@/components/ResultDisplay";
-import { Header } from "@/components/Header";
 
 export const Route = createFileRoute("/")({
   component: App,
