f88a706f99f25cde03d1766cca6356484cd46dac
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-19 11:42:00 +0200

    Added socials to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export const Footer: React.FC = () => {
-
   return (
     <>
       <div
@@ -10,7 +9,29 @@ export const Footer: React.FC = () => {
         }}
         className=" pt-10 pb-8 flex justify-center align-center items-center text-white border-t-2 border-white bg-green-900"
       >
-        <p className="p-4 pr-5 pl-5 border-white border-2 rounded-2xl bg-blue-400">© 2025 Nikita Mokhonko</p>
+        <p className="p-4 pr-5 pl-5 border-white border-2 rounded-2xl bg-blue-400">
+          © 2025 Nikita Mokhonko
+        </p>
+        <a
+          href="https://github.com/NikitaMokhonko"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="w-10 ml-4 mr-4 h-10 border-white border-2 rounded-4xl"
+            src="public/images/github-logo.png"
+          ></img>
+        </a>
+        <a
+          href="https://www.instagram.com/nikitamokhonko/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="w-10 h-10 border-white border-2 rounded-4xl"
+            src="public/images/instagram-logo.png"
+          ></img>
+        </a>
       </div>
     </>
   );
