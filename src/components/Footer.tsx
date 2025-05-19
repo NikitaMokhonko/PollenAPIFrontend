a094981dc30bb1c37690fc9819b357e8945602a6
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-05-19 12:53:20 +0200

    Redesigned footer to work better with newly added socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC = () => {
         }}
         className=" pt-10 pb-8 flex justify-center align-center items-center text-white border-t-2 border-white bg-green-900"
       >
-        <p className="p-4 pr-5 pl-5 border-white border-2 rounded-2xl bg-blue-400">
+        <p className="p-3 pl-2 border-white border-2 rounded-2xl bg-blue-400">
           © 2025 Nikita Mokhonko
         </p>
         <a
